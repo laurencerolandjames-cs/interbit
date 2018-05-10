4b278e085754fee53edc5d98a67e6a65942df000
LaurenceRolandJames <laurencerolandjames@gmail.com>
2018-05-10 10:42:25 +0100

    initial set up app-the-new-thing

---
+++ packages/app-the-new-thing/CHANGELOG.md
@@ -0,0 +1,36 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+<a name="0.4.17"></a>
+## [0.4.17](https://github.com/interbit/interbit/compare/v0.4.16...v0.4.17) (2018-05-08)
+
+
+
+
+**Note:** Version bump only for package template
+
+<a name="0.4.16"></a>
+## [0.4.16](https://github.com/interbit/interbit/compare/v0.4.15...v0.4.16) (2018-05-07)
+
+
+
+
+**Note:** Version bump only for package template
+
+<a name="0.4.15"></a>
+## [0.4.15](https://github.com/interbit/interbit/compare/v0.4.14...v0.4.15) (2018-05-02)
+
+
+
+
+**Note:** Version bump only for package template
+
+<a name="0.4.14"></a>
+## [0.4.14](https://github.com/interbit/interbit/compare/v0.4.13...v0.4.14) (2018-04-27)
+
+
+
+
+**Note:** Version bump only for package template
---
+++ packages/app-the-new-thing/src/interbit/control/index.js
@@ -0,0 +1,74 @@
+// © 2018 BTL GROUP LTD -  This package is licensed under the MIT license https://opensource.org/licenses/MIT
+const { rootCovenant } = require('interbit-covenant-tools')
+const Immutable = require('seamless-immutable')
+
+const { CHAIN_ALIASES, COVENANTS } = require('./constants')
+
+const actionTypes = {}
+
+const actionCreators = {}
+
+const initialState = Immutable.from({
+  // Not required but handy for troubleshooting
+  chainMetadata: { chainName: `Template application - control chain` },
+
+  // Where to find required Interbit services
+  // For security, this information is maintained in the control chain and
+  // shared via a read join to the public chain
+  interbitServices: {
+    // Using shared as a marker to indicate data shared with other chains
+    shared: {
+      // Required for cAuth
+      accounts: {
+        serviceEndpoint:
+          process.env.INTERBIT_ACCOUNTS_URL || 'http://localhost:3025'
+      },
+      // Required for deployment
+      projects: {
+        serviceEndpoint:
+          process.env.INTERBIT_PROJECTS_URL || 'http://localhost:3035'
+      }
+    }
+  },
+
+  // Information required for creating private chains using the
+  // Public/Private/Control (PPC) model
+  privateChainHosting: {}
+})
+
+const reducer = (state = initialState, action) => {
+  const nextState = rootCovenant.reducer(state, action)
+  if (action.type.endsWith('STROBE')) {
+    return nextState
+  }
+
+  switch (action.type) {
+    case rootCovenant.actionTypes.SET_MANIFEST: {
+      const { covenants } = action.payload.manifest
+      if (!covenants) {
+        return nextState
+      }
+
+      const covenantHash = covenants[COVENANTS.PRIVATE]
+
+      return nextState.setIn(
+        ['privateChainHosting', 'shared', CHAIN_ALIASES.PRIVATE],
+        {
+          blockMaster: nextState.getIn(['interbit', 'config', 'blockMaster']),
+          sponsorChainId: nextState.getIn(['interbit', 'chainId']),
+          covenantHash
+        }
+      )
+    }
+
+    default:
+      return nextState
+  }
+}
+
+module.exports = {
+  actionTypes,
+  actionCreators,
+  initialState,
+  reducer
+}
